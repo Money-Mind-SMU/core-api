06a358c2d8f4f4aba08d94380b1662ef7f16b28c
Muhammad Romi Ario Utomo <romi.utomo@bibit.id>
2024-03-04 15:50:19 +0800

    update unit test

--- src/controllers/controllerUser.js
+++ src/controllers/controllerUser.js
@@ -1,38 +1,32 @@
 const jwt = require('../helpers/jwt')
 const {
     User,
-    Pet,
   } = require('../database/models');
 
 class controllerUser{    
-    static login(req,res,next){
-        const{email,password} = req.body
-        User.findOne({ where: { email }})
-            .then(data=>{
-                if(data){
-                    if(data.password === password){
-                        console.log(data)
-                        const {id, name, email, address, photo, phone} = data
-                        req.headers.token = jwt.generateToken({id, name, email, address, photo, phone})
-                        res.json({
-                            token : req.headers.token,
-                            id,
-                            name,
-                            email,
-                            address,
-                            photo,
-                            phone
-                        })
-                    }else{
-                        res.status(404).json({message : 'invalid password/username'})
-                    }  
-                }else{
-                    res.status(404).json({message : 'invalid password/username'})
-                }
-            })
-            .catch(err=>{
-                next(err)
-            })
+    static async updatedProfile(req,res,next){
+        try {
+            const userId = req.user.id; // Assuming the user ID is stored in the decoded JWT token
+            const { name } = req.body;
+        
+            // Validate the request body
+            if (!name) {
+              return res.status(400).json({ error: 'Name is required for updating profile' });
+            }
+        
+            // Update user profile in the database
+            const updatedUser = await User.update({ name }, { where: { id: userId } });
+        
+            // Check if the user was found and updated
+            if (updatedUser[0] === 0) {
+              return res.status(404).json({ error: 'User not found' });
+            }
+        
+            return res.status(200).json({ message: 'Successfully update user profile', data: { name } });
+          } catch (error) {
+            console.error(error);
+            return res.status(500).json({ error: 'Internal Server Error' });
+          }
     }
 
 }
